07e921812de807105bfa1e1b3c82f8131d72bc72
AlexGibby2000 <AlexGibby2000@users.noreply.github.com>
2025-04-07 18:50:24 -0400

    Create deck building page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">
-        Welcome to Pokemon Pocket DeckBuilder!
+        Welcome to Pokemon Trainer Tactics!
       </h1>
       <p className="mt-4 text-lg">This is a simple Next.js application.</p>
     </main>
