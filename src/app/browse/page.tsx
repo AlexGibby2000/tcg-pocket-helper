91a1ff5648568457d6e3c8d3819cc3fcd53af17c
AlexGibby2000 <AlexGibby2000@users.noreply.github.com>
2025-04-07 19:01:38 -0400

    Add proper styling to each page

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Card Browsing Page</h1>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Deck Browser Page</h1>;
+    </main>
+  );
 }
