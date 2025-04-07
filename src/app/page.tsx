a6cd07f66e4a4f96821bbc9449c55b42c17234b6
AlexGibby2000 <AlexGibby2000@users.noreply.github.com>
2025-04-07 16:06:50 -0400

    removed default buttons from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,6 @@ export default function Home() {
       <h1 className="text-4xl font-bold">
         Welcome to Pokemon Pocket DeckBuilder!
       </h1>
-      <Link href="/browse">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">
-          Browse
-        </button>
-      </Link>
-      <Link href="/account">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">
-          Account
-        </button>
-      </Link>
       <p className="mt-4 text-lg">This is a simple Next.js application.</p>
     </main>
   );
