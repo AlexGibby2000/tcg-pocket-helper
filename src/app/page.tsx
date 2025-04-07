fa2c60f94cbcbd8469af797b5bc7b76971d54e9c
AlexGibby2000 <AlexGibby2000@users.noreply.github.com>
2025-04-07 15:36:14 -0400

    Create Initial Page for account

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">
         Welcome to Pokemon Pocket DeckBuilder!
       </h1>
+      <Link href="/browse">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+          Browse
+        </button>
+      </Link>
+      <Link href="/account">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+          Account
+        </button>
+      </Link>
       <p className="mt-4 text-lg">This is a simple Next.js application.</p>
     </main>
   );
