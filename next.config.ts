fa4a0842a1e772247f0b96a22af8805b7d45068d
AlexGibby2000 <AlexGibby2000@users.noreply.github.com>
2025-08-01 13:38:01 -0400

    Starting from scratch

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-const { withClerkMiddleware } = require("@clerk/nextjs/server");
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
